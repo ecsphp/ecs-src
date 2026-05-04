8de456384d4320ff8b5c4527fe2d0f185195e936
Tomas Votruba <tomas.vot@gmail.com>
2026-05-04 11:12:28 +0200

    build build-ecs-scoped

--- build/build-ecs-scoped.sh
+++ build/build-ecs-scoped.sh
@@ -7,8 +7,6 @@ export TERM=xterm-color
 
 # show errors
 set -e
-
-# script fails if trying to access to an undefined variable
 set -u
 
 
@@ -20,31 +18,32 @@ note()
     echo "\033[0;33m[NOTE] $MESSAGE\033[0m";
 }
 
+
+# configure here
+BUILD_DIRECTORY=$1
+RESULT_DIRECTORY=$2
+
+
 # ---------------------------
 
 # 2. scope it -
-note "Downloading php-scoper 0.18.10"
-# released 2023-12
-wget https://github.com/humbug/php-scoper/releases/download/0.18.10/php-scoper.phar -N --no-verbose
+note "Downloading php-scoper 0.18.17"
+wget https://github.com/humbug/php-scoper/releases/download/0.18.17/php-scoper.phar -N --no-verbose
 
-note "Running php-scoper"
-
-# Work around possible PHP memory limits
-php -d memory_limit=-1 php-scoper.phar add-prefix bin config src vendor composer.json --config scoper.php --force --ansi --output-dir scoped-code
-
-# the output code is in "/scoped-code", lets move it up
-# the local directories have to be empty to move easily
-rm -r bin config src vendor composer.json
-mv scoped-code/* .
-
-note "Show prefixed files"
-ls -l .
+note "Running php-scoper on /bin, /config/, /src, /vendor and composer.json"
+php -d memory_limit=-1 php-scoper.phar add-prefix bin config src vendor composer.json --output-dir "../$RESULT_DIRECTORY" --config scoper.php --force --ansi --working-dir "$BUILD_DIRECTORY";
 
 note "Dumping Composer Autoload"
-composer dump-autoload --ansi --classmap-authoritative --no-dev
+composer dump-autoload --working-dir "$RESULT_DIRECTORY" --ansi --classmap-authoritative --no-dev
 
-# make bin/ecs runnable without "php"
-chmod 777 "bin/ecs"
-chmod 777 "bin/ecs.php"
+rm -rf "$BUILD_DIRECTORY"
+
+# copy metafiles needed for release
+note "Copy metafiles like composer.json, .github etc to repository"
+rm -f "$RESULT_DIRECTORY/composer.json"
+
+# make bin files runnable without "php"
+chmod 777 "$RESULT_DIRECTORY/bin/ecs"
+chmod 777 "$RESULT_DIRECTORY/bin/ecs.php"
 
 note "Finished"
