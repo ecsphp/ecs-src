ae08b664191166a7432e7e4b567024d5e88bf4fb
Tomas Votruba <tomas.vot@gmail.com>
2026-05-04 12:43:29 +0200

    use PHP 7.2 syntax

--- build/target-repository/tests-released/config/some_ecs.php
+++ build/target-repository/tests-released/config/some_ecs.php
@@ -5,4 +5,7 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
-    ->withPreparedSets(psr12: true);
+    ->withSets([
+        // PHP 7.2+ compatible config, to enable PHP 7.2 tests
+        \Symplify\EasyCodingStandard\ValueObject\Set\SetList::PSR_12
+    ]);
